00d6db430e5ddf906551f5d741de47b68e994a5e
Atif Imal <mehmetatif@mail.ru>
2024-03-08 23:40:44 +0300

    Added project via Spring Initializer

---
+++ src/main/java/com/atifimal/demo/couriertrack/CourierTrackApplication.java
@@ -0,0 +1,13 @@
+package com.atifimal.demo.couriertrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atifimal/demo/couriertrack/CourierTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atifimal.demo.couriertrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
